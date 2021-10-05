ed539762f8a011088b3217054568fe76f4143392
Quyen Truong <quyen.truong0202@gmail.com>
2021-10-05 09:38:41 -0700

    Update WAN.sh

--- WAN.sh
+++ WAN.sh
@@ -127,15 +127,13 @@ else
     print_servers "dns"
 fi
 
-echo ""
-echo_bold "Conclusion:"
-print_servers "conclusion"
-
 echo -e "$GREEN_LINE"
 echo -e " $(grep '^PRETTY' /etc/os-release | cut -d "=" -f2 | cut -d '"' -f2) $GREEN_SEPARATOR $(date "+%H:%M - %a %m/%d/%Y")"
 echo -e "$GREEN_LINE"
 
 echo -e "$GREEN_BULLET WAN IP $GREEN_SEPARATOR $(curl -sSfLm 3 https://freegeoip.app/csv/ 2>&1 | mawk -F, '($5){r=$5" "}{print $1" "r$3}')"
 echo -e "$GREEN_BULLET Weather $GREEN_SEPARATOR $(curl -sSfLm 3 https://wttr.in/?format=4 2>&1)"
+
+echo -e "$GREEN_BULLET Conclusion $GREEN_SEPARATOR $(print_servers "conclusion")"
 
 echo -e "$GREEN_LINE"
