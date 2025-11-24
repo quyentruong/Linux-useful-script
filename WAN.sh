e20f2f5b2090e754637f6927ce455d05a5e6f60d
Quyen Truong <quyen.truong0202@gmail.com>
2025-11-23 22:35:40 -0800

    Update WAN.sh

--- WAN.sh
+++ WAN.sh
@@ -1,9 +1,10 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 RED='\033[0;31m'
 BOLD='\033[1m'
 NC='\033[0m'
 api_domain='bash.ws'
+api_domain2='ipinfo.io'
 error_code=1
 
 COLOUR_RESET='\e[0m'
@@ -33,8 +34,9 @@ function program_exit {
     increment_error_code
 }
 
-function check_internet_connection {
-    curl --silent --head  --request GET "https://${api_domain}" | grep "200 OK" > /dev/null
+function check_internet_connection () {
+    local api_domain="$1"
+    curl --silent --head  --request GET "https://${api_domain}" | grep "200" > /dev/null
     if [ $? -ne 0 ]; then
         echo_error "No internet connection."
         exit $error_code
@@ -44,13 +46,10 @@ function check_internet_connection {
 
 program_exit curl
 program_exit ping
-check_internet_connection
+program_exit jq
 
-if command -v jq &> /dev/null; then
-    jq_exists=1
-else
-    jq_exists=0
-fi
+check_internet_connection "$api_domain"
+check_internet_connection "$api_domain2"
 
 if hash shuf 2>/dev/null; then
     id=$(shuf -i 1000000-9999999 -n 1)
@@ -63,50 +62,18 @@ for i in $(seq 1 10); do
 done
 
 function print_servers {
-
-    if (( $jq_exists )); then
-
-        echo ${result_json} | \
-            jq  --monochrome-output \
-            --raw-output \
-            ".[] | select(.type == \"${1}\") | \"\(.ip)\(if .country_name != \"\" and  .country_name != false then \" [\(.country_name)\(if .asn != \"\" and .asn != false then \" \(.asn)\" else \"\" end)]\" else \"\" end)\""
-
-    else
-
-        while IFS= read -r line; do
-            if [[ "$line" != *${1} ]]; then
-                continue
-            fi
-
-            ip=$(echo $line | cut -d'|' -f 1)
-            code=$(echo $line | cut -d'|' -f 2)
-            country=$(echo $line | cut -d'|' -f 3)
-            asn=$(echo $line | cut -d'|' -f 4)
-
-            if [ -z "${ip// }" ]; then
-                 continue
-            fi
-
-            if [ -z "${country// }" ]; then
-                 echo "$ip"
-            else
-                 if [ -z "${asn// }" ]; then
-                     echo "$ip [$country]"
-                 else
-                     echo "$ip [$country, $asn]"
-                 fi
-            fi
-        done <<< "$result_txt"
-
-    fi
+    echo ${result_json} | \
+        jq  --monochrome-output \
+        --raw-output \
+        ".[] | select(.type == \"${1}\") | \"\(.ip)\(if .country_name != \"\" and  .country_name != false then \" [\(.country_name)\(if .asn != \"\" and .asn != false then \" \(.asn)\" else \"\" end)]\" else \"\" end)\""
 }
 
+result_json=$(curl --silent "https://${api_domain}/dnsleak/test/${id}?json")
 
-if (( $jq_exists )); then
-    result_json=$(curl --silent "https://${api_domain}/dnsleak/test/${id}?json")
-else
-    result_txt=$(curl --silent "https://${api_domain}/dnsleak/test/${id}?txt")
-fi
+
+function print_servers2 {
+    curl -sSfLm 3 -s $api_domain2 2>&1 | jq -r '"\(.ip) [\(.country), \(.org)]"'
+}
 
 dns_count=$(print_servers "dns" | wc -l)
 
@@ -114,17 +81,21 @@ echo -e "$GREEN_LINE"
 echo -e " $(grep '^PRETTY' /etc/os-release | cut -d "=" -f2 | cut -d '"' -f2) $GREEN_SEPARATOR $(date "+%H:%M - %a %m/%d/%Y")"
 echo -e "$GREEN_LINE"
 
-echo -e "$GREEN_BULLET 1st Test WAN IP $GREEN_SEPARATOR $(curl -sSfLm 3 https://freegeoip.live/csv/ 2>&1 | mawk -F, '($5){r=$5" "}{print $1" "r$3}')"
+echo -e "$GREEN_BULLET 1st Test WAN IP $GREEN_SEPARATOR $(print_servers2)"
 echo -e "$GREEN_BULLET 2nd Test WAN IP $GREEN_SEPARATOR $(print_servers "ip")"
 
 if [ ${dns_count} -eq "0" ];then
     echo -e "$GREEN_BULLET DNS server(s) $GREEN_SEPARATOR No DNS servers found" 
+elif [ ${dns_count} -eq 1 ]; then
+    # Print single DNS server on the same line
+    echo -e "$GREEN_BULLET DNS server(s) $GREEN_SEPARATOR $(print_servers "dns")"
 else
-    echo -e "$GREEN_BULLET DNS server(s) $GREEN_SEPARATOR $(print_servers "dns")" 
+    # Print header, then each DNS server indented
+    echo -e "$GREEN_BULLET DNS server(s) $GREEN_SEPARATOR"
+    print_servers "dns" | while read -r line; do
+        echo -e "    $line"
+    done
 fi
-
-
-echo -e "$GREEN_BULLET Weather $GREEN_SEPARATOR $(curl -sSfLm 3 https://wttr.in/?format=4 2>&1)"
 
 echo -e "$GREEN_BULLET Conclusion $GREEN_SEPARATOR $(print_servers "conclusion")"
 
